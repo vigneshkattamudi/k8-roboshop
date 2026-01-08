f54573419c5240f1385ee771ae2b1539f85acac8
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2026-01-08 09:47:18 +0000

    kubernetes

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+kubectl apply -f 01-namespace
+
+kubectl apply -f mongodb
+kubectl apply -f mysql
+kubectl apply -f redis
+kubectl apply -f rabbitmq
+
+kubectl apply -f catalogue
+kubectl apply -f user
+kubectl apply -f cart
+kubectl apply -f shipping
+kubectl apply -f payment
+kubectl apply -f frontend
+
+kubectl apply -f debug
